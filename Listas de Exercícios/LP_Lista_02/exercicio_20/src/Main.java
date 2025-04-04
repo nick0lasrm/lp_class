0de1e2e09eaee954c6cf8bc072bd8cad41743682
nick0lasrm <nickolasraphael3232@gmail.com>
2025-04-04 19:34:23 -0300

    List02_Ex20

--- Listas de Exercícios/LP_Lista_02/exercicio_20/src/Main.java
+++ Listas de Exercícios/LP_Lista_02/exercicio_20/src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        double b, c , x1, x2;
+
+        System.out.println("Digite os valores de X1 e X2 separados por espaços: ");
+
+        Scanner in = new Scanner(System.in);
+        x1 = in.nextDouble();
+        x2 = in.nextDouble();
+
+        b = (-x1)+(-x2);
+        c = (-x1)*(-x2);
+
+        System.out.println("x² + " + b + "x + " + c + " = 0");
     }
 }

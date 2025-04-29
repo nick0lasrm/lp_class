7bb66e03ea535e2cc2c0bfa205ea6ac780b278f7
nick0lasrm <nickolasraphael3232@gmail.com>
2025-04-28 22:16:41 -0300

    Lista05_Ex02

--- Listas de Exercícios/LP_Lista_05/exercicio-01/src/Main.java
+++ Listas de Exercícios/LP_Lista_05/exercicio-01/src/Main.java
@@ -1,9 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        int [] nums = {0,1,2,3,4,5,6,7,8,9,10};
+        int [] nums = new int [10];
+        nums[0]=1;
+        nums[1]=2;
+        nums[2]=3;
+        nums[3]=4;
+        nums[4]=5;
+        nums[5]=6;
+        nums[6]=7;
+        nums[7]=8;
+        nums[8]=9;
+        nums[9]=10;
 
         for (int i=10;i>0;i--){
-            System.out.println(nums[i]);
+            System.out.println(i);
         }
     }
 }
--- Listas de Exercícios/LP_Lista_05/exercicio-02/src/Main.java
+++ Listas de Exercícios/LP_Lista_05/exercicio-02/src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int [] nums = new int [10];
+        nums[0]=1;
+        nums[1]=2;
+        nums[2]=3;
+        nums[3]=4;
+        nums[4]=5;
+        nums[5]=6;
+        nums[6]=7;
+        nums[7]=8;
+        nums[8]=9;
+        nums[9]=10;
+
+        for (int num : nums){
+            if (num % 2 == 0) {
+                System.out.println(num + 2);
+            }else {
+                System.out.println(num * 2);
+            }
+        }
     }
 }

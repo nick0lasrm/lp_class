b6f6cdc06083a47f8de9543c19fa6c41f0fa83a3
nick0lasrm <nickolasraphael3232@gmail.com>
2025-05-12 22:47:20 -0300

    Lista05_Ex03

--- Listas de Exercícios/LP_Lista_05/exercicio-08/src/Main.java
+++ Listas de Exercícios/LP_Lista_05/exercicio-08/src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int [][] matriz = {{1, 2, 3, 4, 5},
+                            {5, 4, 3, 2, 1},
+                            {6, 7, 8, 9, 10},
+                            {10, 9, 8, 7, 6},
+                            {11, 12, 13, 14, 15}};
+
+        for (int i=0;i<matriz.length;i++){
+            System.out.println(matriz[i][i]);
+        }
     }
 }
